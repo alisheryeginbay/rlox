92f59fcf38a86e6d2e286ef83aaf9b9040e93e92
Alisher <194268626+alisheryeginbay@users.noreply.github.com>
2025-11-25 00:38:26 +0500

    chore: regenerate expr ast

--- src/expr.rs
+++ src/expr.rs
@@ -1,25 +1,25 @@
 use crate::token::*;
 
 pub enum Expr {
-	Literal {
-		value: Literal,
-	},
-	Grouping {
-		expression: Box<Expr>,
+	Binary {
+		left: Box<Expr>,
+		operator: Token,
+		right: Box<Expr>,
 	},
 	Ternary {
 		condition: Box<Expr>,
 		positive: Box<Expr>,
 		negative: Box<Expr>,
 	},
-	Binary {
-		left: Box<Expr>,
-		operator: Token,
-		right: Box<Expr>,
-	},
 	Unary {
 		operator: Token,
 		right: Box<Expr>,
+	},
+	Grouping {
+		expression: Box<Expr>,
+	},
+	Literal {
+		value: Literal,
 	},
 }
 
