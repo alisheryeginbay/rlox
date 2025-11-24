2953ceea9961588372731649852e3239d6dc1af5
Alisher <194268626+alisheryeginbay@users.noreply.github.com>
2025-11-25 01:26:16 +0500

    fix: parse error line and message order

--- src/parser.rs
+++ src/parser.rs
@@ -20,7 +20,7 @@ pub struct Parser {
 
 impl Display for ParseError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "[line {}] {}", self.message, self.line)
+        write!(f, "[line {}] {}", self.line, self.message)
     }
 }
 
